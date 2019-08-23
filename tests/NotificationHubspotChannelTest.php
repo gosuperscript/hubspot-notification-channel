e8974cf076c9b0f50a3e975c2e63d7c192f892de
KaniRobinson <kani_westham@hotmail.com>
2019-08-23 16:24:59 +0100

    Added Test for Notification Channel

--- tests/NotificationHubspotChannelTest.php
+++ tests/NotificationHubspotChannelTest.php
@@ -2,17 +2,54 @@
 
 namespace DigitalRisks\Tests\Notifications;
 
+use DigitalRisks\Notifications\Channels\HubspotChannel;
+use DigitalRisks\Notifications\Messages\HubspotMessage;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notification;
+use Mockery;
 use PHPUnit\Framework\TestCase;
+use SevenShores\Hubspot\Factory;
 
 class NotificationHubspotChannelTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    public function test_email_is_sent_via_hubspot()
     {
-        $this->assertTrue(true);
+        $notification = new HubspotChannelTestNotification;
+        $notifiable = new HubspotChannelTestNotifiable;
+        $channel = new HubspotChannel(
+            $hubspot = Mockery::mock(Factory::class),
+            'kani.robinson@digitalrisks.co.uk',
+        );
+
+        $hubspot->shouldReceive('message->send');
+
+        $channel->send($notifiable, $notification);
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+}
+
+class HubspotChannelTestNotifiable
+{
+    use Notifiable;
+
+    public $email = 'kani.robinson@digitalrisks.co.uk';
+}
+
+class HubspotChannelTestNotification extends Notification
+{
+    public function toHubspot($notifiable)
+    {
+        return (new HubspotMessage)
+            ->templateId('12134249933')
+            ->customProperties([
+                [
+                    'name' => 'name',
+                    'value' => 'Kani Robinson',
+                ],
+            ]);
     }
 }
